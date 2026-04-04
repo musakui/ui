58d3e6629623110e68ce038cd38c44e41708fcf4
Shun Git <kwokshungit@gmail.com>
2026-04-04 13:34:16 +0800

    use tuples and indexed access to nodes

--- lib/tag/types.d.ts
+++ lib/tag/types.d.ts
@@ -1,31 +1,11 @@
-import type { PersistentFragment } from '../util/persistentFragment'
+import type { PersistentFragment } from '#/util/persistentFragment'
 
-type AttrBind = {
-	t: 'a'
-	n: string
-	el: Element
-}
-
-type RawBind = {
-	t: 'r'
-	el: Element
-}
-
-type ContentBind = {
-	t: 'c'
-	el: PersistentFragment
-}
+type RawBind = readonly [t: 'r', el: Element]
+type AttrBind = readonly [t: 'a', el: Element, n: string]
+type ContentBind = readonly [t: 'c', el: PersistentFragment]
 
 export type TemplateBinding = AttrBind | RawBind | ContentBind
 
-type BindHandler<T extends TemplateBinding> = (b: T) => (v: unknown) => void
+type CachedBind = readonly [path: number[], type: 'r' | 'a' | 'c', name?: string]
 
-export type BindHandlers = {
-	r: BindHandler<RawBind>
-	a: BindHandler<AttrBind>
-	c: BindHandler<ContentBind>
-}
-
-type CachedBind = [idx: number, type: 'r'] | [idx: number, type: 'a', name: string]
-
-export type BoundTemplate = readonly [frag: DocumentFragment, binds?: CachedBind[][]]
+export type BoundTemplate = readonly [frag: DocumentFragment, binds?: CachedBind[]]
